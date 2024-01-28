682fd1606767ea095136136b6ca8b55433ce1273
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-01-28 21:21:01 +0200

    initial commit - convert files to kotlin

---
+++ src/main/java/me/_olios/itemcompressor/ItemCompressor.kt
@@ -0,0 +1,13 @@
+package me._olios.itemcompressor
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ItemCompressor : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
